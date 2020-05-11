2d0066ac983df831e006225696ddceb5ffe54926
Anton Shelenkov <anshelen@yandex.ru>
2020-05-11 16:16:35 +0300

    feat: Add logging

--- src/main/java/dev/shelenkov/microservicesbackend/web/RequestsCounterController.java
+++ src/main/java/dev/shelenkov/microservicesbackend/web/RequestsCounterController.java
@@ -1,17 +1,23 @@
 package dev.shelenkov.microservicesbackend.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import static net.logstash.logback.marker.Markers.append;
+
 @RestController
+@Slf4j
 public class RequestsCounterController {
 
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/requests")
     public Long getRequestsCount() {
-        return counter.incrementAndGet();
+        long result = counter.incrementAndGet();
+        log.info(append("Request", result), "Request counter incremented");
+        return result;
     }
 }
